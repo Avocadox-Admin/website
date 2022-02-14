f94a9bfbdc0663d5e89d98f5033db9f9155c8ee3
Jose Miguel Garcia <ti.admin@avocadoxmx.com>
2022-02-13 22:31:00 -0600

    fix: Top Header

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../assets/avocadox_logo.png';
 import { ReactComponent as BurgerIcon } from '../assets/burgerIcon.svg';
 
@@ -10,16 +11,23 @@ const routeLinks = [
 ];
 
 const TopBar = () => {
+  const location = useLocation();
+  console.log(location);
+
   return (
     <div className="flex absolute top-0 w-full h-[100px] bg-black/70 text-white drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] items-center z-10">
       <div className="px-12 grow-0">
-        <img className="max-w-[135px]" src={logo} alt="logo"></img>
+        <Link to="/">
+          <img className="max-w-[135px]" src={logo} alt="logo" />
+        </Link>
       </div>
       <div className="grow flex justify-end">
-        <div className="grow flex">
-          <ul className="flex w-36 uppercase text-center">
+        <div className="grow flex justify-end">
+          <ul className="flex uppercase text-center">
             {routeLinks.map((link) => (
-              <li key={link.label}>{link.label}</li>
+              <li className="w-36" key={link.label}>
+                {link.label}
+              </li>
             ))}
           </ul>
         </div>
