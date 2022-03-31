e02b1e45b05a0cb8b9e1cc624da8c0d6d75ef951
Miguel Garcia <ti.admin@avocadoxmx.com>
2022-03-31 11:23:39 -0600

    Home page elements

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,16 +9,16 @@ import PhoneIcon from '../images/phoneIcon.inline.svg';
 const Footer = () => {
   return (
     <div className="grid grid-col-1 grid-row-3 bg-[#ACC465] text-white">
-      <div>
-        <WhatsappIcon />
-        <InstagramIcon />
-        <FacebookIcon />
+      <div className="flex justify-center gap-2 mt-4">
+        <WhatsappIcon className="h-6 w-6" />
+        <InstagramIcon className="h-6 w-6" />
+        <FacebookIcon className="h-6 w-6" />
       </div>
-      <div>
-        <EmailIcon />
-        <PhoneIcon />
+      <div className="flex justify-center mt-4">
+        <EmailIcon className="h-6 w-6" />
+        <PhoneIcon className="h-6 w-6" />
       </div>
-      <div>Disclaimer / Politica de Privacidad</div>
+      <div className="flex justify-center mt-4 mb-2">Disclaimer / Politica de Privacidad</div>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,77 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+
 import Layout from '../components/Layout';
+import { Link } from 'gatsby';
 
 const Home = () => {
   return (
     <Layout>
-      <div className="mt-[100px]">Hello from Avocadox</div>
+      <div className="mt-[100px]">
+        <section className="px-12 pb-12">
+          <div className="text-2xl font-black mb-4">Desarrollo Sustentable</div>
+          <div className="mb-4">
+            ¿Cuál es es el impacto de lo que consumes en el ambiente y en tu grupo local?
+          </div>
+          <div className="mb-8">
+            Nuestra empresa esta realmente comprometida con el cuidado del planeta y el
+            <strong> BIENESTAR</strong> de tu país, ciudad grupo de amigos y de tu familia. Tú eres
+            parte de este esfuerzo y compromiso.
+          </div>
+          <button
+            className="bg-black text-white h-12 rounded-full px-4"
+            onClick={() => console.log('Button clicked')}>
+            ¿Sabes cómo?
+          </button>
+        </section>
+        <section className="bg-black text-white px-12 pb-12">
+          <div className="text-2xl font-black mb-4">Etapas del Producto</div>
+          <div className="mb-4">
+            El aguacate <strong>HASS</strong> es una fuente de riquezas e incomparable sabor y
+            beneficios para la salud y nutrición.
+          </div>
+          <div className="mb-12">
+            Nuestra oferta de exportación de frescos (empacados), <strong>PASTA</strong>,{' '}
+            <strong>GUACAMOLE</strong> y <strong>ACEITE</strong>.
+          </div>
+          <div className="flex items-center gap-4 mb-8">
+            <StaticImage src="../images/frescos.png" alt="Frescos" />
+            <span>Frescos</span>
+          </div>
+          <div className="flex items-center gap-4 mb-8">
+            <StaticImage src="../images/aceite.png" alt="Aceite" />
+            <span>Aceite</span>
+          </div>
+          <div className="flex items-center gap-4">
+            <StaticImage src="../images/guacamole.png" alt="Pasta y Guacamole" />
+            <span>Pasta y Guacamole</span>
+          </div>
+        </section>
+        <div className="h-8 bg-[#ACC465]"></div>
+        <section className="bg-black text-white px-12 pb-12">
+          <div className="text-2xl font-black">Certificaciones</div>
+          <div className="grid grid-cols-3">
+            <Link to="/certificaciones">
+              <StaticImage src="../images/fairtrade-negative.png" alt="Fairtrade Logo" />
+            </Link>
+            <Link to="/certificaciones">
+              <StaticImage src="../images/sagarpa-negative.png" alt="Sagarpa Logo" />
+            </Link>
+            <Link to="/certificaciones">
+              <StaticImage src="../images/usda-negative.png" alt="USDA Logo" />
+            </Link>
+            <Link to="/certificaciones" className="col-span-2">
+              <StaticImage src="../images/primus-negative.png" alt="Primus GFS Logo" />
+            </Link>
+            <Link to="/certificaciones">
+              <StaticImage src="../images/globalGap-negative.png" alt="Global Gap Logo" />
+            </Link>
+            <Link to="/certificaciones" className="col-span-3">
+              <StaticImage src="../images/esr-negative.png" alt="ESR Logo" />
+            </Link>
+          </div>
+        </section>
+      </div>
     </Layout>
   );
 };
